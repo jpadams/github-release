172fc21cbec24c97a797b0a2286bce640e773c5c
Kyle Penfound <kyle@dagger.io>
2023-11-01 21:54:02 -0400

    add create func
    Signed-off-by: Kyle Penfound <kyle@dagger.io>

--- main.go
+++ main.go
@@ -12,6 +12,42 @@ const (
 
 type GithubRelease struct {}
 
+func (g *GithubRelease) Create(
+		ctx context.Context,
+		repo string,
+		tag string,
+		title string,
+		token *Secret,
+		notes Optional[string],
+		draft Optional[bool],
+		latest Optional[bool],
+		prerelease Optional[bool],
+	) (string, error) {
+		createCmd := []string{"release", "create", tag}
+
+		notes_, isset := notes.Get()
+		if isset {
+			createCmd = append(createCmd, fmt.Sprintf("'%s'", notes_))
+		}
+
+		if draft.GetOr(false) {
+			createCmd = append(createCmd, "--draft")
+		}
+
+		if latest.GetOr(false) {
+			createCmd = append(createCmd, "--latest")
+		}
+
+		if prerelease.GetOr(false) {
+			createCmd = append(createCmd, "--prerelease")
+		}
+		return ghImage().
+		WithSecretVariable("GH_TOKEN", token).
+		WithEnvVariable("GH_REPO", repo).
+		WithExec(createCmd).
+		Stdout(ctx)
+}
+
 func (g *GithubRelease) CreateWithAssets(
 		ctx context.Context,
 		repo string,
@@ -22,7 +58,7 @@ func (g *GithubRelease) CreateWithAssets(
 		notes Optional[string],
 		draft Optional[bool],
 		latest Optional[bool],
-		prerelease Optional[bool]
+		prerelease Optional[bool],
 	) (string, error) {
 		createCmd := []string{"release", "create", tag}
 
@@ -54,6 +90,6 @@ func (g *GithubRelease) CreateWithAssets(
 func ghImage() *Container {
 	return dag.Container().
 	From(RUNTIME).
-	WithFile("/bin/gh", dag.Gh().Get(GH_CLI_VERSION)).
+	WithFile("/bin/gh", dag.Gh().Get(GhGetOpts{ Version: GH_CLI_VERSION})).
 	WithEntrypoint([]string{"/bin/gh"})
 }
